36ff3a373bd3258f636d4609830376ee78a13360
Regis Mucyo <mucyore@gmail.com>
2022-03-16 01:11:22 -0700

    DO IT NOW

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * main - Prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+ int _putchar(char c)
+ {
+ 	
+  return (write(1,&c, 1));
+ }
+ int main (void)
+ {
+ 	_putchar('_');
+ 	_putchar('p');
+ 	_putchar('u');
+ 	_putchar('t');
+ 	_putchar('c');
+ 	_putchar('h');
+ 	_putchar('a');
+ 	_putchar('r');
+ 	_putchar('\n');
+ 	return(0);
+ }
